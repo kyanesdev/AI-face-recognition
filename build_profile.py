b97e0d230f9b1be7ca3f2b1eec10bf6931cccd68
kyanesdev <kyanesdev@gmail.com>
2023-07-24 23:58:14 -0300

    Flujo completo terminado

--- build_profile.py
+++ build_profile.py
@@ -2,7 +2,7 @@ import cv2
 import face_recognition
 import numpy as np
 
-file = "kevin.jpg"  # Replace with the path to the image you want to use for building the profile
+file = "new_user.jpg"  # Replace with the path to the image you want to use for building the profile
 
 image = cv2.imread(file)
 
@@ -10,4 +10,4 @@ faces_loc = face_recognition.face_locations(image)
 
 for fl in faces_loc:
     modelo = face_recognition.face_encodings(image, known_face_locations=[fl])
-    np.savetxt("perfil.txt", modelo)
+    np.savetxt("saved_user.txt", modelo)
